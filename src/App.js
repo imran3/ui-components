db642b54d598dee986395a4fb3607785dcf50b24
Imran Azam <imran.azam@and.digital>
2022-02-17 23:21:08 +0000

    added button component, style etc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "./lib/components/Button";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "start",
+        justifyContent: "center",
+        gap: "1.5rem",
+        padding: "5rem",
+      }}
+    >
+      <p>Should i push this button?</p>
+      <Button label="Enter" kind="primary" />
     </div>
   );
 }
